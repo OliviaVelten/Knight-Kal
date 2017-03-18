d09ffd07e39ee114927e4726cf6418578c36e8ea
OliviaVelten <olivia.velten-lomelin@menloschool.org>
2017-03-17 20:18:55 -0700

    add swift project

---
+++ Knight Kal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Knight Kal
+//
+//  Created by Olivia Velten on 3/17/17.
+//  Copyright © 2017 Olivia Velten. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
